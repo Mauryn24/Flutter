b4092c36f3f5b6d4848bfaff8a24d831bcc8b09f
Mauryn24 <maurynnyakio19@gmail.com>
2024-05-25 23:37:09 +0300

    flutter widgets

--- flutter_app/flutter_widgets/lib/main.dart
+++ flutter_app/flutter_widgets/lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
+    // Home page 
+      home: Center(
+
+        // padding nesting
+        child: Padding(
+          padding: const EdgeInsets.all(30.0), // padding
+
+          // text
           child: Text(
             'Hello World!',
+
+            // style
             style: TextStyle(
+
+              // text color
               color: Colors.greenAccent,
+
+              // background color
               backgroundColor: Colors.black54,
+
+              // cross line through a text
+              decoration: TextDecoration.lineThrough,
               ),
             ),
         ),
