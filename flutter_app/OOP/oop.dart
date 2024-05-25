3515a9cf125bd912f1db37df4f23a5ffe0ed17b2
Mauryn24 <maurynnyakio19@gmail.com>
2024-05-25 22:16:24 +0300

    dart OOP

--- flutter_app/OOP/oop.dart
+++ flutter_app/OOP/oop.dart
@@ -4,10 +4,26 @@ void main() {
   print(person.address);
 }
 
+// terms in oop
+//  - keyword class
+// -  object - an instance of a class
+//  - Inheritance
+//  - Polymorphism
+//  - Abstraction
+//  - Encapsulation
+
 class Person {
+  // attributes/characteristics
   String name;
   int age;
   String? address;
 
+  // methods - they give behaviour to class
+  void printInfo() {
+    print(name);
+    print(age);
+  }
+
+  // Constructor
   Person(this.name, this.age, [this.address = 'Kite']);
 }
