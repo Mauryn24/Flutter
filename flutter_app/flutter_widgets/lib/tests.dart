d33de309570b446df77b8079bd1dab91e6faefd9
Mauryn24 <maurynnyakio19@gmail.com>
2024-05-26 21:17:23 +0300

    flutter widgets

--- flutter_app/flutter_widgets/lib/tests.dart
+++ flutter_app/flutter_widgets/lib/tests.dart
@@ -1,6 +1,6 @@
 void main() {
   // main
-  Person person = Person('john', 40);
+  Person person = Person('john', 40, 'Nakuru','0712345678', 'Kenya');
   print(person.address);
   person.printInfo();
   // printing a string in dart
@@ -18,9 +18,11 @@ void main() {
 
 class Person {
   // attributes/characteristics
-  String name;
-  int age;
-  String? address;
+  late String name;
+  late int age;
+  late String? address;
+  late String? phonenumber;
+  late String? Country;
 
   // methods - they give behaviour to class
   void printInfo() {
@@ -28,13 +30,24 @@ class Person {
     print(age);
   }
 
+  Person(String firstname, int age, String address,String country, String phone){
+  
+  this.name = 'Dr $firstname';
+  this.age = age;
+  this.Country = country;
+  this.phonenumber = phone;
+}
   // Constructor
   // this.name - link your name to the name attribute
-  Person(this.name, this.age, [this.address = 'Kite']);
+  // Person(this.name, this.age, this.phonenumber, this.Country
+  //     [this.address = 'Nairobi, Kenya']);
 }
 
+
 // inheritance
-class Teacher extends Person{
-  // Person is the parent class/superclass
-  // Teacher is the child/subclass
-}
+// class Teacher extends Person {
+//   // Person is the parent class/superclass
+//   // Teacher is the child/subclass
+
+//   // child constructor
+// }
