e4f68d36750d22b04e6444e226648a0c47647ef6
Mauryn24 <maurynnyakio19@gmail.com>
2024-06-03 00:05:01 +0300

    Register and login page

--- flutter_app/fooddeliveryapp/lib/pages/login_page.dart
+++ flutter_app/fooddeliveryapp/lib/pages/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:fooddeliveryapp/components/my_textfield.dart';
 import 'package:fooddeliveryapp/components/my_button.dart';
 
@@ -14,77 +13,84 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
-
   final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // logo
-            Icon(
-              Icons.lock_open_rounded,
-              size: 100,
-              color: Theme.of(context).colorScheme.inversePrimary,
-            ),
-            const SizedBox(height: 25),
-            // message, app slogan
-            Text(
-              "Food Delivery App",
-              style: TextStyle(
-                fontSize: 16,
-                color: Theme.of(context).colorScheme.inversePrimary,
-              ),
-            ),
-            const SizedBox(height: 25),
-            // email textfield
-            MyTextfield(
-              controller: emailController,
-              hintText: "Email",
-              obsecureText: false,
-            ),
-            const SizedBox(height: 10),
-            // password textfield
-            MyTextfield(
-              controller: passwordController,
-              hintText: "Password",
-              obsecureText: true,
-            ),
-            const SizedBox(height: 10),
-            // sign in button
-            MyButton(
-              text: "Sign In",
-              onTap: () {},
-            ),
-            const SizedBox(height: 10),
-            // not a member? register now
-            Row(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Not a member?",
-                style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+                const SizedBox(height: 50),
+                // logo
+                Icon(
+                  Icons.lock_open_rounded,
+                  size: 100,
+                  color: Theme.of(context).colorScheme.inversePrimary,
                 ),
-                const SizedBox(width: 4),
-                
-                GestureDetector(
-                onTap: widget.onTap,
-                  child: Text(
-                    "Register Now",
-                    style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary,
-                  fontWeight: FontWeight.bold),
-                  
+                const SizedBox(height: 25),
+                // message, app slogan
+                Text(
+                  "Food Delivery App",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.inversePrimary,
                   ),
                 ),
-                
-                
+                const SizedBox(height: 25),
+                // email textfield
+                MyTextfield(
+                  controller: emailController,
+                  hintText: "Email",
+                  obsecureText: false,
+                ),
+                const SizedBox(height: 10),
+                // password textfield
+                MyTextfield(
+                  controller: passwordController,
+                  hintText: "Password",
+                  obsecureText: true,
+                ),
+                const SizedBox(height: 20),
+                // sign in button
+                MyButton(
+                  text: "Sign In",
+                  onTap: () {},
+                ),
+                const SizedBox(height: 20),
+                // not a member? register now
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Not a member?",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                      ),
+                    ),
+                    const SizedBox(width: 4),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: Text(
+                        "Register Now",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.inversePrimary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 50),
               ],
-              
             ),
-          ],
+          ),
         ),
       ),
     );
--- flutter_app/fooddeliveryapp/lib/pages/register_page.dart
+++ flutter_app/fooddeliveryapp/lib/pages/register_page.dart
@@ -12,92 +12,93 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController confirmpasswordController = TextEditingController();
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // logo
-            Icon(
-              Icons.lock_open_rounded,
-              size: 100,
-              color: Theme.of(context).colorScheme.inversePrimary,
-            ),
-            const SizedBox(height: 25),
-            // message, app slogan
-            Text(
-              "Let's create an account for you",
-              style: TextStyle(
-                fontSize: 16,
-                color: Theme.of(context).colorScheme.inversePrimary,
-              ),
-            ),
-
-            const SizedBox(height: 25),
-            // email textfield
-            MyTextfield(
-              controller: emailController,
-              hintText: "Email",
-              obsecureText: false,
-            ),
-
-            const SizedBox(height: 10),
-            // password textfield
-            MyTextfield(
-              controller: passwordController,
-              hintText: "Password",
-              obsecureText: true,
-            ),
-
-            const SizedBox(height: 10),
-            // confirmpassword textfield
-            MyTextfield(
-              controller: confirmpasswordController,
-              hintText: "Confirm Password",
-              obsecureText: true,
-            ),
-
-            const SizedBox(height: 10),
-            // sign Up button
-            MyButton(
-              text: "Sign Up",
-              onTap: () {},
-            ),
-            const SizedBox(height: 10),
-            // Already Have an Account? Login here
-
-            Row(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Already Have an Account?",
-                style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+                const SizedBox(height: 50),
+                // logo
+                Icon(
+                  Icons.lock_open_rounded,
+                  size: 100,
+                  color: Theme.of(context).colorScheme.inversePrimary,
                 ),
-                const SizedBox(width: 4),
-                
-                GestureDetector(
-                onTap: widget.onTap,
-                  child: Text(
-                    "Login Here",
-                    style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary,
-                  fontWeight: FontWeight.bold),
-                  
+                const SizedBox(height: 25),
+                // message, app slogan
+                Text(
+                  "Let's create an account for you",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.inversePrimary,
                   ),
                 ),
-                
-                
+                const SizedBox(height: 25),
+                // email textfield
+                MyTextfield(
+                  controller: emailController,
+                  hintText: "Email",
+                  obsecureText: false,
+                ),
+                const SizedBox(height: 10),
+                // password textfield
+                MyTextfield(
+                  controller: passwordController,
+                  hintText: "Password",
+                  obsecureText: true,
+                ),
+                const SizedBox(height: 10),
+                // confirm password textfield
+                MyTextfield(
+                  controller: confirmpasswordController,
+                  hintText: "Confirm Password",
+                  obsecureText: true,
+                ),
+                const SizedBox(height: 20),
+                // sign up button
+                MyButton(
+                  text: "Sign Up",
+                  onTap: () {},
+                ),
+                const SizedBox(height: 20),
+                // already have an account? login here
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Already have an account?",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                      ),
+                    ),
+                    const SizedBox(width: 4),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: Text(
+                        "Login Here",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.inversePrimary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 50),
               ],
-              
             ),
-          ],
+          ),
         ),
       ),
     );
