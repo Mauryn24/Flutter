26b7e801345211edb27b034accbf3c67ff76dabc
Mauryn24 <maurynnyakio19@gmail.com>
2024-06-03 02:06:04 +0300

    dark and light mode

--- flutter_app/fooddeliveryapp/lib/main.dart
+++ flutter_app/fooddeliveryapp/lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:fooddeliveryapp/auth/login_or_register.dart';
+import 'package:provider/provider.dart';
 import 'pages/login_page.dart';
 import 'pages/register_page.dart';
+import 'package:fooddeliveryapp/theme_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+     ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+   child: const MyApp(),
+  ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,9 +20,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child){
+        return MaterialApp(
+          theme: themeProvider.isDarkMode ? ThemeData.dark() : ThemeData.light(),
       home: const LoginOrRegister(),
     );
+      },
+    );
+    
   }
 }
 
--- flutter_app/fooddeliveryapp/lib/pages/settings_page.dart
+++ flutter_app/fooddeliveryapp/lib/pages/settings_page.dart
@@ -1,14 +1,51 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:fooddeliveryapp/theme_provider.dart'; // Import the ThemeProvider class
 
 class SettingsPage extends StatelessWidget {
-  const SettingsPage({super.key});
+  const SettingsPage({Key? key}) : super(key: key); // Add const and update key parameter
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Settings"),
-        ),
-        );
+        title: const Text("Settings"),
+        backgroundColor: Theme.of(context).colorScheme.background,
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            margin: const EdgeInsets.only(left: 25, top: 10, right: 25),
+            padding: const EdgeInsets.all(25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Dark Mode
+                Text(
+                  "Dark Mode",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+                // Switch
+                CupertinoSwitch(
+                  value: Provider.of<ThemeProvider>(context).isDarkMode,
+                  onChanged: (value) {
+                    Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
