76c99b26803e5a7b6635cc175ed3ab1e16ec917f
Mauryn24 <maurynnyakio19@gmail.com>
2024-07-08 23:03:04 +0300

    Contacts app

--- flutter_app/contacts_app/lib/ui/contacts_list/favourites_page.dart
+++ flutter_app/contacts_app/lib/ui/contacts_list/favourites_page.dart
@@ -6,7 +6,9 @@ class FavouritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text('Favourites'),
+      ),
     );
   }
 }
