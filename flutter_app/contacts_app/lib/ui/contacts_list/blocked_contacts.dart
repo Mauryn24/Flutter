5964895ef251e2a796b6ef382eaa7608e7a41200
Mauryn24 <maurynnyakio19@gmail.com>
2024-07-28 22:29:51 +0300

    Contacts app

---
+++ flutter_app/contacts_app/lib/ui/contacts_list/blocked_contacts.dart
@@ -0,0 +1,9 @@
+import 'contacts_list_page.dart';
+class BlockedContacts extends StatelessWidget {
+  const BlockedContacts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
